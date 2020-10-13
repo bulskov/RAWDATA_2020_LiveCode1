03d93d143965f279890682ae7620fb41f1ef8752
Henrik Bulskov <bulskov@ruc.dk>
2020-10-13 12:05:50 +0200

    Extension method

--- LanguageBasics/Program.cs
+++ LanguageBasics/Program.cs
@@ -2,13 +2,24 @@
 
 namespace LanguageBasics
 {
-    class Util
+    static class Util
     {
         public static int a = 5;
+
+        public static string Reverse(this string s)
+        {
+            string res = "";
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                res += s[i];
+            }
+            return res;
+        }
     }
     class Program
     {
-
+        static public int X { get; set; }
         static void Swap(ref int x, ref int y)
         {
             int tmp = x;
@@ -19,6 +30,18 @@ namespace LanguageBasics
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+
+            string name = "Peter";
+
+            //string res = Util.Reverse(name);
+
+            string res = name.Reverse();
+
+            Console.WriteLine(res);
+
+            return;
+
+            Program.X = 5;
 
             var a = 5;
             var b = 6;
